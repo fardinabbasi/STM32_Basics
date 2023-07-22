3bcfa1638bedd44639c616cd4ff1d156edbf34c3
Fardin Abbasi <fardinabbasi1381@gmail.com>
2023-07-22 12:40:44 +0330

    Add LCD

---
+++ LCD/PartB/Debug/Core/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/lcd.c 
+
+OBJS += \
+./Core/lcd.o 
+
+C_DEPS += \
+./Core/lcd.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/%.o Core/%.su Core/%.cyclo: ../Core/%.c Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core
+
+clean-Core:
+	-$(RM) ./Core/lcd.cyclo ./Core/lcd.d ./Core/lcd.o ./Core/lcd.su
+
+.PHONY: clean-Core
+
